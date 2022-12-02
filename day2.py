f0a8f2573e2c541073e4df118765c49f6771da30
Juergen Hoetzel <juergen@hoetzel.info>
2022-12-02 20:57:19 +0100

    Update: Part 2

--- day2.py
+++ day2.py
@@ -15,5 +15,15 @@ def score(opponent, me):
     return SCORES[opponent][me] + points(me)
 
 
+SCORES2 = {
+    "A": {"Y": 1 + 3, "X": 3 + 0, "Z": 2 + 6},
+    "B": {"Y": 2 + 3, "X": 1 + 0, "Z": 3 + 6},
+    "C": {"Y": 3 + 3, "X": 2 + 0, "Z": 1 + 6},
+}
+
+
 input_pairs = [line.split(" ") for line in Path("input2.txt").read_text().splitlines()]
-print(sum([score(opponent, me) for (opponent, me) in input_pairs]))
+part1 = sum([score(opponent, me) for (opponent, me) in input_pairs])
+part2 = sum([SCORES2[opponent][strategy] for (opponent, strategy) in input_pairs])
+print(f"Part1: {part1}")
+print(f"Part2: {part2}")

9932ff157d6d75cf35e214ffcd6c25a1acb8ac98
Juergen Hoetzel <juergen@hoetzel.info>
2022-12-03 08:41:19 +0100

    Add part2

--- day3.py
+++ day3.py
@@ -7,12 +7,28 @@ def priority(c):
     return ord(c) - ord("a") + 1
 
 
-print(
-    sum(
-        [
-            priority(*(set(line[:s]) & set(line[s:])))  # should be exactile one
-            for line in Path("input3.txt").read_text().splitlines()
-            if (s := len(line) // 2)
-        ]
-    )
+part1 = sum(
+    [
+        priority(*(set(line[:s]) & set(line[s:])))  # should be exactile one
+        for line in Path("input3.txt").read_text().splitlines()
+        if (s := len(line) // 2)
+    ]
 )
+print(f"Part1: {part1}")
+
+lines = [
+    line
+    for line in Path("input3.txt").read_text().splitlines()
+    if (s := len(line) // 2)
+]
+
+
+part2 = sum(
+    [
+        priority(*set.intersection(*[set(sack) for sack in lines[i : (i + 3)]]))
+        for i in range(0, len(lines), 3)
+    ]
+)
+
+
+print(f"Part2: {part2}")

65da22aef4cb576cd1f9e90b20c474659b0decce
Juergen Hoetzel <juergen@hoetzel.info>
2022-12-06 16:18:00 +0100

    Code cleanup: Useless use of zip

--- day6.py
+++ day6.py
@@ -1,11 +1,7 @@
 from pathlib import Path
 
 
-def interleave_message(s, n=4):
-    return zip(*[line[i:] for i in range(n)])
-
-
 line = Path("input6.txt").read_text().splitlines()[0]
-offs = [[len(set(xs)) for xs in interleave_message(line, n)].index(n) for n in (4, 14)]
-print(f"Part1: {offs[0]+4}")
-print(f"Part2: {offs[1]+14}")
+for p, n in ((1, 4), (2, 14)):
+    offset = [len(set(line[i : i + n])) for i in range(len(line) - n)].index(n)
+    print(f"Part{p}: {offset+n}")

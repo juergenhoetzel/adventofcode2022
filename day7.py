8007624ce90a6c9c003cb97a274f155a3f5488e6
Juergen Hoetzel <juergen@hoetzel.info>
2022-12-10 09:28:46 +0100

    Add day 7 part 1

---
+++ day7.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import re
+
+SIZE_RE = re.compile("([0-9]+) (.*)$")
+DIR_RE = re.compile("dir (.*)$")
+COMMAND_RE = re.compile(r"(ls|cd )(.*)$")
+size = 0
+p = Path("/")
+sizes = {}
+current_command = None
+
+s = Path("input7.txt").read_text()
+
+cmd_outputs = re.split(r"\$ ", s, flags=re.MULTILINE)[1:]
+
+dir_tree = {Path("/"): []}
+cur_path = Path("/")
+
+for cmd_output in cmd_outputs:
+    cmd_s = cmd_output.splitlines()[0]
+    output_lines = cmd_output.splitlines()[1:]
+    m = COMMAND_RE.match(cmd_s)
+    cmd, arg = m.groups()
+    if cmd == "ls":
+        if len(dir_tree.get(cur_path, [])):
+            raise ValueError(f"{cur_path} entries already exist")
+        dir_tree[cur_path] = []
+        for output in output_lines:
+            if m := SIZE_RE.match(output):
+                size, name = int(m.group(1)), m.group(2)
+                dir_tree[cur_path].append((size, name))
+            elif m := DIR_RE.match(output):
+                name = m.group(1)
+                dir_tree[cur_path].append((0, name))
+            else:
+                raise ValueError("Invalid ls line {output}")
+    elif cmd == "cd ":
+        cur_path = (cur_path / arg).resolve()
+        dir_tree[cur_path] = dir_tree.get(cur_path, [])
+    else:
+        raise ValueError(f"Invalid Command '{cmd}'")
+
+
+def dir_size(p):
+    acc = 0
+    for size, name in dir_tree.get(p):
+        if size:
+            acc += size
+        else:
+            acc += dir_size(p / Path(name))
+    return acc
+
+
+print(sum([x for p in dir_tree.keys() if (x := dir_size(p)) <= 100_000]))
